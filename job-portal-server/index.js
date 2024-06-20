bca714f4b1addbbf9e2336985b71f617af621638
Jyothika Lakshmi M <jyothikalakshmim07@gmail.com>
2024-06-20 22:24:37 +0530

    connected to mongodb

--- job-portal-server/index.js
+++ job-portal-server/index.js
@@ -1,10 +1,44 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT ||5000;
+const port = process.env.PORT ||3002;
+require('dotenv').config()
+console.log(process.env.db_user)
+console.log(process.env.db_passwd)
 
 app.use(express.json())
 app.use(cors())
+
+// username: jyothikalakshmim07
+// password: QMd3CtO7H0J8zsg8
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_passwd}@job-portal-demo.tmuwq60.mongodb.net/?retryWrites=true&w=majority&appName=job-portal-demo`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello developer!')
@@ -13,3 +47,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+// ensures mongodb client connection is properly closed when server is stopped
+process.on('SIGINT', async () => {
+    console.log('Shutting down server...');
+    await client.close();
+    process.exit(0);
+  });
